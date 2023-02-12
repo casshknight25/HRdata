c9262069af516e738e12ba34ee7acd6e7a03d45f
casshknight25 <100038929+casshknight25@users.noreply.github.com>
2023-02-12 20:08:04 +0000

    Update hrdata.py

--- hrdata.py
+++ hrdata.py
@@ -31,8 +31,8 @@ if analysis =='Predicting Leavers EDA':
     st.components.v1.html(html_data,height=17000)
 
 if analysis =='Leavers Deep Dive':
-    fig = px.bar(df, x="Department", y="count", color="Left")
-    st.plotly_chart(fig, use_container_width=True)
+    lbc = alt.Chart(df).mark_bar().encode(alt.X("Departent"),y="count()", color="Left")
+    st.altair_chart(lbc, use_container_width=True)
     fig1 = px.box(leftdf,x='Department', y="Age")
     st.plotly_chart(fig1, use_container_width=True)
     fig2 = px.box(leftdf,x = 'Department', y="YearsSinceLastPromotion")
